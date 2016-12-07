3f6a4226ac95d5fd20df441fbfc96c1d9c368526
Marco Martin <notmart@gmail.com>
2016-12-07 13:19:06 +0100

    minimal tool for install
    just a frontend for the root helper, to be used by kns

---
+++ src/kplymouththemeinstaller.cpp
@@ -0,0 +1,78 @@
+/*
+ *  Copyright (C) 2016 Marco Martin <mart@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#include "config-kcm.h"
+
+#include <QCoreApplication>
+#include <QDir>
+#include <QFile>
+#include <QDebug>
+#include <QProcess>
+#include <qcommandlineparser.h>
+#include <qcommandlineoption.h>
+
+#include <klocalizedstring.h>
+#include <kauthaction.h>
+#include <kauthexecutejob.h>
+
+int main(int argc, char **argv)
+{
+    QCommandLineParser parser;
+    QCoreApplication app(argc, argv);
+
+    const QString description = i18n("Plymouth theme installer");
+    const char version[] = "1.0";
+
+    app.setApplicationVersion(version);
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.setApplicationDescription(description);
+    parser.addPositionalArgument("themefile", i18n("The theme to install, must be an existing archive file."));
+
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+
+    if (args.isEmpty()) {
+        qWarning() << "No theme file specified.";
+        return 0;
+    }
+
+    const QString themefile = args.first();
+    if (!QFile::exists(themefile)) {
+        qWarning() << "Specified theme file does not exists";
+        return 0;
+    }
+
+    QVariantMap helperargs;
+    helperargs[QStringLiteral("theme")] = themefile;
+    KAuth::Action action(QStringLiteral("org.kde.kcontrol.kcmplymouth.install"));
+    action.setHelperId("org.kde.kcontrol.kcmplymouth");
+    action.setArguments(helperargs);
+
+    KAuth::ExecuteJob *job = action.execute();
+    bool rc = job->exec();
+    if (!rc) {
+        qWarning() << i18n("Unable to authenticate/execute the action: %1, %2", job->error(), job->errorString());
+        return -1;
+    }
+
+    qWarning()<< themefile;
+    return app.exec();
+}
