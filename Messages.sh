2614ef366a2f04acecbef7f853c2b9c4f22a2b5e
Marco Martin <notmart@gmail.com>
2016-12-30 10:36:04 +0100

    fix messages

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/plymouth_sddm.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/plymouth_kcm.pot
 rm -f rc.cpp
