aca25cd6895caa00f8a9186b3be5a4beb1898a29
Pino Toscano <pino@kde.org>
2020-08-28 10:05:45 +0200

    cmake: fix search for pkg-config
    properly use find_package() to search for it, making it required (as we
    query for pkg-config variables)

--- cmake/FindPlymouth.cmake
+++ cmake/FindPlymouth.cmake
@@ -21,7 +21,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-include(FindPkgConfig)
+find_package(PkgConfig REQUIRED)
 
 pkg_check_modules(Plymouth ply-boot-client ply-splash-core)
 exec_program(${PKG_CONFIG_EXECUTABLE}
